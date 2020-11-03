8fff88dfe3f876d4092d30a382870eef0c21eb31
Kieran Farr <kieran.farr@gmail.com>
2020-11-02 21:24:40 -0800

    update event to trigger anisotropy setting on matierals without model-loaded

--- index.js
+++ index.js
@@ -142,29 +142,54 @@ AFRAME.registerComponent('anisotropy', {
     this.maxAnisotropy = this.el.sceneEl.renderer.capabilities.getMaxAnisotropy();
     // console.log('this.maxAnisotropy', this.maxAnisotropy);
 
-    this.el.addEventListener('model-loaded', () => {
-      const mesh = this.el.getObject3D('mesh');
-      // console.log('mesh', mesh);
+    ['model-loaded', 'materialtextureloaded'].forEach(evt =>
+      this.el.addEventListener(evt, () => {
+        const mesh = this.el.getObject3D('mesh');
+        // console.log('mesh', mesh);
 
-      var anisotropyTargetValue = this.data;
-      anisotropyTargetValue = +anisotropyTargetValue || 0; // https://stackoverflow.com/questions/7540397/convert-nan-to-0-in-javascript
-      // console.log('anisotropyTargetValue', anisotropyTargetValue);
-
-      if (anisotropyTargetValue === 0) {
-        anisotropyTargetValue = this.maxAnisotropy;
+        var anisotropyTargetValue = this.data;
+        anisotropyTargetValue = +anisotropyTargetValue || 0; // https://stackoverflow.com/questions/7540397/convert-nan-to-0-in-javascript
         // console.log('anisotropyTargetValue', anisotropyTargetValue);
-      }
 
-      mesh.traverse((object) => {
-        if (object.isMesh === true && object.material.map !== null) {
-          // console.log('object', object);
-          // console.log('object.material.map.anisotropy', object.material.map.anisotropy);
-          object.material.map.anisotropy = anisotropyTargetValue;
-          // console.log('object.material.map.anisotropy', object.material.map.anisotropy);
-          object.material.map.needsUpdate = true;
+        if (anisotropyTargetValue === 0) {
+          anisotropyTargetValue = this.maxAnisotropy;
+          // console.log('anisotropyTargetValue', anisotropyTargetValue);
         }
-      });
-    });
+
+        mesh.traverse((object) => {
+          if (object.isMesh === true && object.material.map !== null) {
+            // console.log('object', object);
+            // console.log('object.material.map.anisotropy', object.material.map.anisotropy);
+            object.material.map.anisotropy = anisotropyTargetValue;
+            // console.log('object.material.map.anisotropy', object.material.map.anisotropy);
+            object.material.map.needsUpdate = true;
+          }
+        });
+      }, false)
+    );
+    // this.el.addEventListener('model-loaded', () => {
+    //   const mesh = this.el.getObject3D('mesh');
+    //   // console.log('mesh', mesh);
+
+    //   var anisotropyTargetValue = this.data;
+    //   anisotropyTargetValue = +anisotropyTargetValue || 0; // https://stackoverflow.com/questions/7540397/convert-nan-to-0-in-javascript
+    //   // console.log('anisotropyTargetValue', anisotropyTargetValue);
+
+    //   if (anisotropyTargetValue === 0) {
+    //     anisotropyTargetValue = this.maxAnisotropy;
+    //     // console.log('anisotropyTargetValue', anisotropyTargetValue);
+    //   }
+
+    //   mesh.traverse((object) => {
+    //     if (object.isMesh === true && object.material.map !== null) {
+    //       // console.log('object', object);
+    //       // console.log('object.material.map.anisotropy', object.material.map.anisotropy);
+    //       object.material.map.anisotropy = anisotropyTargetValue;
+    //       // console.log('object.material.map.anisotropy', object.material.map.anisotropy);
+    //       object.material.map.needsUpdate = true;
+    //     }
+    //   });
+    // });
   }
 });
 
